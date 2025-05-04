b6ea44acbb451f9fa102a15c1f2f7c29380e8ff6
Yusuf <121219831+Gadaffi508@users.noreply.github.com>
2025-05-04 10:33:06 +0300

    fixed

--- ServerProject/Program.cs
+++ ServerProject/Program.cs
@@ -1,43 +1,46 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
-class Program
+namespace BasicTcpServer
 {
-    static void Main()
+    class Program
     {
-        TcpListener server = new TcpListener(IPAddress.Any, 10000);
-        server.Start();
-        Console.WriteLine("Server başlatıldı...");
-
-        while (true)
+        static void Main()
         {
-            TcpClient client = server.AcceptTcpClient();
-            Console.WriteLine("Bir istemci bağlandı.");
+            TcpListener server = new TcpListener(IPAddress.Any, 10000);
+            server.Start();
+            Console.WriteLine("Server başlatıldı...");
 
-            Thread thread = new Thread(() => HandleClient(client));
-            thread.Start();
-        }
-    }
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Bir istemci bağlandı.");
 
-    static void HandleClient(TcpClient client)
-    {
-        NetworkStream stream = client.GetStream();
-        byte[] buffer = new byte[1024];
-        int byteCount;
-
-        while ((byteCount = stream.Read(buffer, 0, buffer.Length)) != 0)
-        {
-            string msg = Encoding.UTF8.GetString(buffer, 0, byteCount);
-            Console.WriteLine("Gelen: " + msg);
-
-            string response = "Sunucudan: " + msg.ToUpper();
-            byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-            stream.Write(responseBytes, 0, responseBytes.Length);
+                Thread thread = new Thread(() => HandleClient(client));
+                thread.Start();
+            }
         }
 
-        client.Close();
+        static void HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int byteCount;
+
+            while ((byteCount = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                string msg = Encoding.UTF8.GetString(buffer, 0, byteCount);
+                Console.WriteLine("Gelen: " + msg);
+
+                string response = "Sunucudan: " + msg.ToUpper();
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                stream.Write(responseBytes, 0, responseBytes.Length);
+            }
+
+            client.Close();
+        }
     }
 }
